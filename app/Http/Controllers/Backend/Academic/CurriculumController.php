28a3117babca7c2df40ce6b8b58da311957364f7
AgungMahardhika <m.agungmahardhika@yahoo.co.id>
2020-05-19 00:39:10 +0700

    Update CurriculumController.php
    update curriculum controller

--- app/Http/Controllers/Backend/Academic/CurriculumController.php
+++ app/Http/Controllers/Backend/Academic/CurriculumController.php
@@ -28,18 +28,31 @@ class CurriculumController extends Controller
      */
     public function create()
     {
-        //
-    }
+        $departments = Department::all()->pluck('name', 'id');
 
+        return view('klp09.curricula.create', compact('departments'));
+    }
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+public function store(Request $request)
     {
-        //
+        if(!Gate::allows('staffs_manage'))
+        {
+            return abort(403);
+        }
+
+        $request->validate(Curriculum::validation_rules);
+
+        if(Curriculum::create($request->all())){
+            notify('success', 'Berhasil menambahkan data Kurikulum');
+        }else{
+            notify('error', 'Gagal menambahkan data Kurikulum');
+        }
+        return redirect()->route('backend.curricula.index');
     }
 
     /**
