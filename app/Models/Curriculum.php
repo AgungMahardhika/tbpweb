e58b1dcfd1be292ff28fb5c29b5d2af2ca6f6a19
ringgoagsya <63402411+ringgoagsya@users.noreply.github.com>
2020-05-18 21:18:26 +0700

    Update Curriculum.php

--- app/Models/Curriculum.php
+++ app/Models/Curriculum.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Curriculum extends Model
 {
-    //
+   protected $guarded = [];
+    
+    const validation_rules = [
+        'name' => 'required',
+        'department_id' => 'required',
+    ];
+    public function departement()
+    {
+        return $this->belongsTo(Department::class,'department_id','id');
+    }
 }
